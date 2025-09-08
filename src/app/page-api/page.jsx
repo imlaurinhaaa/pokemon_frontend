9659ee53118a550e29ee7e30217a43ad307f13ad
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2025-09-07 22:13:03 -0300

    feat: add the api

--- src/app/page-api/page.jsx
+++ src/app/page-api/page.jsx
@@ -1,21 +1,68 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./api.module.css";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useState, useEffect } from "react";
+import { Spin, Pagination } from "antd";
 
 export default function PageAPI() {
+    const [pokemons, setPokemons] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [pageSize, setPageSize] = useState(8);
+
+    const fetchPokemons = async () => {
+        setLoading(true);
+        try {
+            const response = await axios.get(
+                "https://pokeapi.co/api/v2/pokemon?limit=50&offset=0"
+            );
+            const detailedPokemons = await Promise.all(
+                response.data.results.map(async (pokemon) => {
+                    const res = await axios.get(pokemon.url);
+                    return res.data;
+                })
+            );
+
+            setPokemons(detailedPokemons);
+            toast.success("Pokemons carregados com sucesso!", {
+                toastId: "success-load",
+            });
+        } catch (error) {
+            console.error("Erro ao buscar pokemons:", error);
+            toast.error("Erro ao carregar pokemons.", {
+                toastId: "error-load",
+            });
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchPokemons();
+    }, []);
+
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    const currentPokemons = pokemons.slice(startIndex, endIndex);
+
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
+
+    const handlePageSizeChange = (current, size) => {
+        setPageSize(size);
+        setCurrentPage(1);
+    };
+
+
     return (
         <div className={styles.container}>
             <header className={styles.header}>
                 <Image src="/images/pokemon.png" alt="Logo" width={250} height={250} />
-                <ul className={styles.navLinks}>
-                    <nav className={styles.nav}>
-                        <Link href="/">Home</Link>
-                    </nav>
-                    <nav className={styles.nav}>
-                        <Link href="/page-project">Sobre API</Link>
-                    </nav>
-                </ul>
             </header>
             <div className={styles.content}>
                 <div className={styles.textSection}>
@@ -30,17 +77,77 @@ export default function PageAPI() {
                         explorar dados relacionados aos Pokémon, seja para jogos, sites ou
                         projetos de análise.
                     </p>
+                    <div className={styles.buttonSection}>
+                        <Link
+                            href="/"
+                            className={styles.button}
+                        >
+                            Página Inicial
+                        </Link>
+                        <Link
+                            href="/page-api"
+                            className={styles.button}
+                        >
+                            Pokemon API
+                        </Link>
+                    </div>
                 </div>
                 <div className={styles.imageSection}>
                     <Image
-                        src="/images/personagens.png"
+                        src="/images/pikachu.gif"
                         alt="Imagem Personagens de Pokemon"
-                        width={500}
+                        width={300}
                         height={300}
                         className={styles.image}
                     />
                 </div>
+
             </div>
+            {loading ? (
+                <div className={styles.loadingContainer}>
+                    <Image
+                        src="/images/loading.gif"
+                        alt="Imagem de carregamento"
+                        width={300}
+                        height={300}
+                        className={styles.loadingImage} />
+                    <p className={styles.loadingText}>Carregando Pokemons...</p>
+                </div>
+            ) : (
+                <>
+                    <div className={styles.pagination}>
+                        <Pagination
+                            total={pokemons.length}
+                            pageSize={pageSize}
+                            current={currentPage}
+                            showSizeChanger={true}
+                            pageSizeOptions={["8", "16", "20"]}
+                            onChange={handlePageChange}
+                            onShowSizeChange={handlePageSizeChange}
+                        />
+                    </div>
+
+                    <div className={styles.cardsContainer}>
+                        {currentPokemons.map((pokemon, index) => (
+                            <div
+                                key={index}
+                                className={styles.pokemonCard}
+                            >
+                                <div className={styles.cardContent}>
+                                    <img
+                                        src={pokemon.sprites.front_default}
+                                        alt={pokemon.name}
+                                        className={styles.pokemonImage}
+                                    />
+                                    <h3 className={styles.pokemonName}>{pokemon.name}</h3>
+                                    <p className={styles.pokemonInfo}>Tipo: {pokemon.types.map((t) => t.type.name).join(", ")}</p>
+
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </>
+            )}
         </div>
     );
 }
